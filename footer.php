7b180b06bfacee47a1bb9a8a9dfce18a40635333
Jens Langhammer <jens@ad.beryju.org>
2015-10-27 16:33:02 +0100

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,19 @@
+        <!-- Footer -->
+        <footer id="footer">
+<!--             <ul class="icons">
+                <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+                <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+                <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+            </ul> -->
+        </footer>
+
+        <!-- Scripts -->
+        <script src="<?php bloginfo('template_directory') ?>/assets/js/jquery.min.js"></script>
+        <script src="<?php bloginfo('template_directory') ?>/assets/js/jquery.poptrox.min.js"></script>
+        <script src="<?php bloginfo('template_directory') ?>/assets/js/skel.min.js"></script>
+        <script src="<?php bloginfo('template_directory') ?>/assets/js/util.js"></script>
+        <!--[if lte IE 8]><script src="<?php bloginfo('template_directory') ?>/assets/js/ie/respond.min.js"></script><![endif]-->
+        <script src="<?php bloginfo('template_directory') ?>/assets/js/main.js"></script>
+    </body>
+</html>
