887cd77502e39a22085629445bb33e948fe58637
Jens Langhammer <jens@ad.beryju.org>
2015-11-03 14:36:18 +0100

    removed old files, switched to static css files

--- common.inc.php
+++ common.inc.php
@@ -3,12 +3,25 @@ class Common {
 
 	public static $BLOGINFO_FIELDS = array("name","description","wpurl","url","admin_email","charset","version","html_type","language","stylesheet_url","stylesheet_directory","template_url","pingback_url","atom_url","rdf_url","rss_url","rss2_url","comments_atom_url","comments_rss2_url");
 
-	public static function get_site_info() {
+	public static function populate_context() {
 		$data = array();
 		foreach (Common::$BLOGINFO_FIELDS as $key) {
 			$data[$key] = get_bloginfo($key);
 		}
+		$data['menu'] = new TimberMenu();
+		$data['home'] = home_url('/');
 		return $data;
 	}
 
+	public static function render($name) {
+		$context = Timber::get_context();
+		$context['site'] = Common::populate_context();
+		$context['posts'] = Timber::get_posts();
+		Timber::render("templates/$name.twig.html", $context);
+	}
+
 }
+
+
+
+add_filter('timber_context', 'Common::populate_context');
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 require_once 'common.inc.php';
-$context = Timber::get_context();
-$context['site'] = Common::get_site_info();
-Timber::render('templates/index.twig.html', $context);
+Common::render("index");
