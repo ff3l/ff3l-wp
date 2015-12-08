1fb6a5141cb303fba9c577c7e191305a299e390e
Jens Langhammer <jens@ad.beryju.org>
2015-12-08 16:17:10 +0100

    Type commit message here…

--- author.php
+++ author.php
@@ -1,7 +1,9 @@
 <?php
 require_once 'functions.php';
 $author = new TimberUser();
+$id = $author->ID;
 Common::render("author", array(
   "author" => $author,
   "author_avatar" => Common::get_gravatar($author->$user_email)
+  "author_posts" => Timber::get_posts("author=$id")
   ));
