013ec2c78ee15b5843d3ef49c29860f73dc5beca
Jens Langhammer <jens@ad.beryju.org>
2015-12-08 16:17:56 +0100

    these commit messages are terrible

--- author.php
+++ author.php
@@ -4,6 +4,6 @@ $author = new TimberUser();
 $id = $author->ID;
 Common::render("author", array(
   "author" => $author,
-  "author_avatar" => Common::get_gravatar($author->$user_email)
+  "author_avatar" => Common::get_gravatar($author->$user_email),
   "author_posts" => Timber::get_posts("author=$id")
   ));
