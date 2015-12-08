57f00ad4793e6aa06f594495e72c30f857fe7554
Jens Langhammer <jens@ad.beryju.org>
2015-12-08 16:20:20 +0100

    pls work

--- author.php
+++ author.php
@@ -5,5 +5,6 @@ $id = $author->ID;
 Common::render("author", array(
   "author" => $author,
   "author_avatar" => Common::get_gravatar($author->$user_email),
+  "author_bio" => get_the_author_meta('description', $id),
   "author_posts" => Timber::get_posts("author=$id")
   ));
