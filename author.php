13cd1799af91c4f31edce19fb2119621d2a82b8a
Jens Langhammer <jens@ad.beryju.org>
2015-12-08 15:46:15 +0100

    this might work

--- author.php
+++ author.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'functions.php';
-$author = new TimberUser(intval($_GET['author']));
+$author = new TimberUser();
 Common::render("author", array(
   "author" => $author,
   "author_avatar" => Common::get_gravatar_url($author->$user_email)
