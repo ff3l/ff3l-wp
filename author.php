3aeaef75fa277adc7abac66e1cb823c4632273d1
Jens Langhammer <jens.langhammer@beryju.org>
2015-12-08 17:10:35 +0100

    avatars should work now

--- author.php
+++ author.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'functions.php';
-$author = new TimberUser();
-$id = $author->ID;
+$author_id = get_queried_object()->ID;
+$author = new TimberUser($author_id);
 Common::render("author", array(
   "author" => $author,
-  "author_avatar" => Common::get_gravatar($author->$user_email),
-  "author_bio" => get_the_author_meta('description', $id),
-  "author_posts" => Timber::get_posts("author=$id")
+  "author_avatar" => Common::get_gravatar(get_userdata($author_id)->user_email)),
+  "author_bio" => get_the_author_meta('description', $author_id),
+  "author_posts" => Timber::get_posts("author=$author_id")
   ));
