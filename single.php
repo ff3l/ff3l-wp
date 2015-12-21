37cf8a9e646dbd7e0f6e7c27550bdb3205ec0270
Jens Langhammer <jens.langhammer@beryju.org>
2015-12-21 12:34:03 +0100

    added special site for calendar's

--- single.php
+++ single.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'functions.php';
-Common::render("single", array(
+$url = "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$tmpl = "single";
+if (preg_match("^http(s?):\/\/(?:[A-Za-z0-9\.]*)\/(c|k)al", $url) == 1) {
+	// We are on a calendar site. Let's remove the author and also the comments
+	$tmpl = "single-cal";
+}
+Common::render($tmpl, array(
 	"post" => new TimberPost()
 	));
