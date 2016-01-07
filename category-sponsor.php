c60a7d3299ab2d47a3c8a599d6402d3c0726c471
Jens Langhammer <jens.langhammer@beryju.org>
2016-01-07 11:05:29 +0100

    randomizing posts on sponsor page

--- category-sponsor.php
+++ category-sponsor.php
@@ -1,3 +1,6 @@
 <?php
 require_once 'functions.php';
-Common::render("sponsor");
+$context = Timber::get_context();
+$context['site'] = Common::populate_context();
+$context['posts'] = shuffle(Timber::get_posts());
+Timber::render("templates/sponsor.twig", $context);
