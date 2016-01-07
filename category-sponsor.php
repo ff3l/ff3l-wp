56c4290805ed9fce26e8cff573fb2f8ffeeb9e0d
Jens Langhammer <jens.langhammer@beryju.org>
2016-01-07 11:06:43 +0100

    god damnit²

--- category-sponsor.php
+++ category-sponsor.php
@@ -3,5 +3,6 @@ require_once 'functions.php';
 $context = Timber::get_context();
 $context['site'] = Common::populate_context();
 $posts = Timber::get_posts();
-$context['posts'] = shuffle($posts);
+shuffle($posts);
+$context['posts'] = $posts;
 Timber::render("templates/sponsor.twig", $context);
