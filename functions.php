65b1a1b926b9232a8db698dd1d05fb3ef0b52499
Malte Kiefer <maltekiefer1987@gmail.com>
2017-09-21 14:26:43 +0000

    header

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 
 function ff3l_scripts() {
+    //include bootstap style
+    //include font awesome style
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), 'v4.0.0-beta' );
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0' );
 	
+	//include bootstap js
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '4.7.0', true);
 
 }
